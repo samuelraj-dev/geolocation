46a9a64d25f061d858e62de63147450b90dd9fb6
samuelraj-dev <samuelraj.dev@outlook.com>
2024-08-13 20:05:38 +0530

    third commit

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://geolocation.topoship.com"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // origin: ["http://localhost:5173", "https://geolocation.topoship.com"],
+    origin: "*",
+    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true,
   })
 );
 
